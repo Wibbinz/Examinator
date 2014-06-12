dec7506865b15dea80e13fceaff0c54f328373b6
Wibbinz <r.iwamoto@shaw.ca>
2014-06-11 21:52:47 -0500

    Created tables

--- Examinator/Examinator.sql
+++ Examinator/Examinator.sql
@@ -11,8 +11,75 @@ GO
 ------------------------------
 --CREATING & POPULATING TABLES
 ------------------------------
+--drop table tbUsers
+create table tbUsers
+(UserID int identity (1000,1) primary key,
+UserName varchar (30),
+UserPass varchar (30),
+UserEmail varchar (30),
+UserLvl int,
+UserBit bit)
+go
+
+--drop table tbPreferences
+create table tbPreferences
+(PrefID int identity (10,1) primary key,
+PrefUserID int foreign key references tbUsers (UserID),
+PrefBkgCol varchar (20), 	-- page background
+PrefBannerCol varchar (20), -- menu bar colour
+PrefBoldCol varchar (20), 	-- headings, questions and anything boldface
+PrefTxtCol varchar (20),	-- descriptions, answers, explanations
+PrefFont varchar (20),		-- page font
+PrefBit bit)
+go
 
 
+--drop table tbCategory
+create table tbCategory
+(CategoryID int identity (100,1) primary key,
+CatName varchar (25),
+CatDesc varchar (65),
+CatBit bit)
+go
+
+--drop table tbQuestions
+create table tbQuestions
+(QuestionID int identity (10000,1) primary key,
+QuestionCatID int foreign key references tbCategory (CategoryID),
+QuestionUploader int foreign key references tbUsers (UserID),
+QuestionTxt varchar (256),
+QuestionImgPath varchar (50),
+QuestionRecTime int,
+QuestionUploadDateTime, datetime,
+QuestionBit bit)
+go
+
+--drop table tbAnswers
+create table tbAnswers
+(AnswerID int identity (10000,1) primary key,
+AnswerQuestionID int foreign key references tbQuestions (QuestionID),
+AnswerCorrect varchar (256),
+Answer1 varchar (256),
+Answer2 varchar (256),
+Answer3 varchar (256),
+Answer4 varchar (256),
+Answer5 varchar (256),
+AnswerCorrectImg varchar (50),
+Answer1Img varchar (50),
+Answer2Img varchar (50),
+Answer3Img varchar (50),
+Answer4Img varchar (50),
+Answer5Img varchar (50),
+AnswerBit bit)
+go
+
+--drop table tbExplanations
+create table tbExplanations
+(ExplnID int identity (1,1) primary key,
+ExplnQuestionID int foreign key references tbQuestions (QuestionID),
+ExplnText varchar (500),
+ExplnBit bit)
+go
 
 ----------------------------
 --STORED PROCEDURES
