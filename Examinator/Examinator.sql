afd1c3f07c7cffee4f498e0f75bad58e4ea1c690
unknown <kirtana.iwamoto@robertsoncollege.net>
2014-06-11 20:31:26 -0500

    Added SQL file

---
+++ Examinator/Examinator.sql
@@ -0,0 +1,30 @@
+USE master
+GO
+DROP DATABASE dbExaminator
+GO
+CREATE DATABASE dbExaminator
+GO
+USE dbExaminator
+GO
+
+
+------------------------------
+--CREATING & POPULATING TABLES
+------------------------------
+
+
+
+----------------------------
+--STORED PROCEDURES
+----------------------------
+
+
+--------------------------------------------------------------
+--Display, Verify, Add, Update and Delete from Table tbUser
+--------------------------------------------------------------
+
+
+
+------------------------
+--TEST QUERIES
+------------------------
