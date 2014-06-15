39fda164583d44c51120a784bad6e4991aa1c223
Wibbinz <r.iwamoto@shaw.ca>
2014-06-15 11:37:08 -0500

    Finished inserts, added sp's for add and verify

--- Examinator/Examinator.sql
+++ Examinator/Examinator.sql
@@ -51,7 +51,7 @@ QuestionUploader int foreign key references tbUsers (UserID),
 QuestionTxt varchar (256),
 QuestionImgPath varchar (50),
 QuestionRecTime int,
-QuestionUploadDateTime, datetime,
+QuestionUploadDateTime datetime,
 QuestionBit bit)
 go
 
@@ -111,11 +111,11 @@ go
 
 --Questions Table
 insert into tbQuestions
-(QuestionIDCat,QuestionUploader,QuestionTxt,QuestionImgPath,QuestionRecTime,QuestionUploadDateTime,QuestionBit)
+(QuestionCatID,QuestionUploader,QuestionTxt,QuestionImgPath,QuestionRecTime,QuestionUploadDateTime,QuestionBit)
 values	(100,1002,'What animal Moos and makes Milk?',NULL,60,'2014-01-01',1),			--10000
 		(100,1002,'What type of bear lives at the North Pole?',NULL,60,'2014-01-01',1),		--10001
 		(100,1000,'What family of animals do Lions belong to?',NULL,60,'2014-01-01',1),		--10002
-		(100,1000,'Snoopy from the cartoon Peanuts is what kind of dog?',NULL,'2014-01-01',1),	--10003
+		(100,1000,'Snoopy from the cartoon Peanuts is what kind of dog?',NULL,60,'2014-01-01',1),	--10003
 		(100,1001,'What feline animal has spots?',NULL,60,'2014-02-02',1),				--10004
 		(104,1003,'Who is the Chief Engineer on the USS Enterprise?',NULL,60,'2014-03-01',1),	--10005
 		(104,1003,'What is the middle name of Captain Kirk?',NULL,60,'2014-03-01',1),		--10006
@@ -136,13 +136,100 @@ values	(10000,'A Cow','A Horse','A Chicken','A Hippopotamus','An Elephant','A Gi
 		(10003,'Beagle','Irish Setter','Daschund','Collie','Foxhound','Badger',NULL,NULL,NULL,NULL,NULL,NULL,1),		--10003
 		(10004,'Leopard','Tiger','Lion','OhMy','Siamese','Cheetah',NULL,NULL,NULL,NULL,NULL,NULL,1),		--10004
 		(10005,'Lt. Commander Scott','Ensign Chekhov','Captain Kirk','Commander Spock','Doctor McCoy','Lt. Commander Uhura',NULL,NULL,NULL,NULL,NULL,NULL,1),		--10005
-		(10006,
+		(10006,'Tiberius','Bartholomew','Flavius','William','Abraham','Socrates',NULL,NULL,NULL,NULL,NULL,NULL,1),		--10006
+		(10007,'He/She will be killed','There will be a new co-star in the series','Captain Kirk will get married','He/She will replace one of the bridge crew','Nothing will happen','He/She will turn out to be a Romulan spy',NULL,NULL,NULL,NULL,NULL,NULL,1),		--10007
+		(10008,'A Tricorder','A Flux Capacitor','A Phaser','A Plasma Conduit','A Transporter','A Photon Torpedo',NULL,NULL,NULL,NULL,NULL,NULL,1),		--10008
+		(10009,'NCC-1701','NCC-5405','UFP-1701','USS-5000','HPD-50','SOS-2001',NULL,NULL,NULL,NULL,NULL,NULL,1),		--10009
+		(10010,'A Shuttlecraft','A Phase Array','A Dilithium Matrix','A Tricorder','A Homing Beacon','A Matter-AntiMatter Pod',NULL,NULL,NULL,NULL,NULL,NULL,1),		--10010
+		(10011,'Vulcan','Klingon','Romulan','Ferrengi','Human','Shapeshifter',NULL,NULL,NULL,NULL,NULL,NULL,1),		--10011
+		(10012,'Lion','Rhinocerous','Grizzly Bear','Elephant','Bengal Tiger','Killer Whale',NULL,NULL,NULL,NULL,NULL,NULL,1)		--10012
+go
 
-
+--Explanations Table (for non-Exam Modes)
+insert into tbExplanations
+(ExplnQuestionID,ExplnText,ExplnBit)
+values	(10000,'A Cow is raised on dairy farms to produce milk and often makes Mooing sounds.',1),		--1
+		(10001,'Polar Bears live in the Artic Circle and have white fur to blend in with the snow.',1),	--2
+		(10002,'Lions belong to the Feline family of animals, typically any animal that is cat-like.',1),	--3
+		(10003,'Snoopy is a Beagle, and was born on the Daisy Hill Puppy Farm.',1),		--4
+		(10005,'The Chief Engineer on the USS Enterprise is Lt. Cmdr Montgomery Scott, also known as "Scotty".',1),	--5
+		(10006,'Captain Kirk''s middle name is Tiberius, he was born in Riverside Iowa on March 22, 2233.',1),		--6
+		(10007,'He/she usually gets killed.  Affectionately nicknamed ''Ensign Nobody'' or ''Ensign Redshirt'', they are typically the first ones killed on an away mission.',1),		--7
+		(10008,'The Tricorder is the general purpose hand-held device used for sensor scanning, data analysis and recording data in the ST universe.',1),		--8
+		(10009,'The designation of the USS Enterprise is NCC-1701, a Constitution class heavy cruiser starship that was launched in 2245.',1),		--9
+		(10010,'A Shuttlecraft is typically used when the Transporter is unable to be used for "beaming down".  It typically carries 2 to 6 persons.',1),		--10
+		(10011,'Mister Spock is a Vulcan, from the planet Vulcan.  The Vulcans are known for their logic, intelligence and ability to suppress emotion.',1),		--11
+		(10012,'The Lion is known as The King of Beasts, in the Panthera genus; may weigh up to 550 pounds and the males are recognizable by their highly distinctive manes.',1)		--12
+go
+	
 ----------------------------
 --STORED PROCEDURES
 ----------------------------
 
+--drop procedure spGetUsers 
+--gets one (by UserID) or all clients
+create procedure spGetUsers
+(
+	@UserID int = null
+)
+as
+	begin
+		select
+		*
+		from tbUsers
+		where UserID = isnull(@UserID,UserID)
+		and UserBit = 1
+	end
+go
+
+--drop procedure spVerifyUsers 
+--used in Login procedure 
+create procedure spVerifyUsers
+(
+	@UserName varchar (30) = null,
+	@UserPass varchar (30) = null
+)
+as
+	begin
+		if exists (select * from tbUsers where UserName = @UserName and UserPass = @UserPass and UserBit = 1)
+			begin
+				select * from tbUsers where UserName = @UserName
+				and UserBit = 1
+			end
+		else
+			begin	
+				select ClientLvl = -1 from tbUsers
+			end
+	end
+go
+
+--drop procedure spAddUsers 
+--procedure to add new users 
+create procedure spAddUsers
+(
+	@UserID int = null,
+	@UserName varchar (30) = null,
+	@UserPass varchar (30) = null,
+	@UserEmail varchar (30) = null,
+	@UserLvl int = null,
+	@UserBit bit = 1
+)
+as
+	begin
+		if exists (select * from tbUsers where UserName = @UserName)
+			begin
+				select 'UserID Exists' = 'UserID Exists' from tbUsers where UserName = @UserName
+			end
+	else
+		begin
+			insert into tbUsers
+			(UserName,UserPass,UserEmail,UserLvl,UserBit)
+			values	(@UserName,@UserPass,@UserEmail,1,1)
+			select * from tbUsers where UserName = @UserName
+			and UserBit = 1
+		end
+	end
+go
 
 --------------------------------------------------------------
 --Display, Verify, Add, Update and Delete from Table tbUser
@@ -153,3 +240,4 @@ values	(10000,'A Cow','A Horse','A Chicken','A Hippopotamus','An Elephant','A Gi
 ------------------------
 --TEST QUERIES
 ------------------------
+
