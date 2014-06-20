d90bbddfe39fa62837eb1151756ec132d9767992
unknown <kirtana.iwamoto@robertsoncollege.net>
2014-06-20 16:45:24 -0500

    Added modal window.

---
+++ Examinator/Examinator/js/BellsandWhistles.js
@@ -0,0 +1,6 @@
+﻿function showDialog() {
+    var newRegs = document.getElementById("newRegs");
+    var overlay = document.getElementById("black_overlay");
+    newRegs.removeAttribute("hidden");
+    overlay.removeAttribute("hidden");
+}
